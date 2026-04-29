26be28f8bdc59b7ae720a508b57b7c3ce7f1bc60
EGrishkina <EGrishkina@croc.ru>
2026-04-29 19:25:23 +0300

    Starter settings

---
+++ src/main/java/com/glizzie/smartportfolioai/SmartPortfolioAiApplication.java
@@ -0,0 +1,13 @@
+package com.glizzie.smartportfolioai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPortfolioAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartPortfolioAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glizzie/smartportfolioai/SmartPortfolioAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glizzie.smartportfolioai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPortfolioAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
